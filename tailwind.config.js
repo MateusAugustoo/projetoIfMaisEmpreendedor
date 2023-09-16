b6d4f4a4949b2f58c08f005efeaadfb4124079c4
Erick Alves <britoerick16@gmail.com>
2023-09-16 20:37:40 -0300

    Adicionado Página Produtos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "comida-fundo": "url('../images/fundoOrange.png')",
+      }),
+      backgroundSize: (theme) => ({
+        "80%": "80%",
+      }),
+      backgroundPosition: (theme) => ({
+        "top-left" : "0px left",
+      }),
+    },
   },
   plugins: [],
-}
-
+};
