ce1f016083273aee8a5f5e162fc055ce6e7a65d1
MateusAugusro <mateusaugusto2017pp@gmail.com>
2023-11-04 03:14:33 -0300

    função para destacar o icons selecionado da drawer

--- public/js/drawer.js
+++ public/js/drawer.js
@@ -10,3 +10,37 @@ menuButton.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   drawer.classList.toggle("-translate-x-full");
 });
+
+
+function handleItemClick(itemId) {
+  const items = document.querySelectorAll('.button');
+
+  items.forEach(item => {
+    if (item.id === itemId) {
+      item.classList.add('bg-white', 'shadow', 'rounded-[40px]', 'left-0', 'top-0', 'absolute');
+
+      const svgElement = item.querySelector('svg');
+
+      if (svgElement) {
+        const pathElements = svgElement.querySelectorAll('path');
+
+        pathElements.forEach(pathElement => {
+          pathElement.setAttribute('stroke', '#D95A11')
+        })
+      }
+    } else {
+      item.classList.remove('bg-white', 'shadow', 'rounded-[40px]', 'left-0', 'top-0', 'absolute');
+
+      const svgElement = item.querySelector('svg');
+
+      if (svgElement) {
+        const pathElements = svgElement.querySelectorAll('path');
+
+        pathElements.forEach(pathElement => {
+          pathElement.setAttribute('stroke', 'white')
+        })
+      }
+    }
+  });
+}
+
