dbb6ce82a755ee4279e64b77f0b7359102c46495
MateusAugusto <mateusaugusto2017pp@gmail.com>
2023-11-12 00:02:15 -0300

    Refactor renderProduct.js to improve code structure and readability

--- public/js/renderProduct.js
+++ public/js/renderProduct.js
@@ -51,9 +51,7 @@ async function renderPizza() {
             window.location.href = `../pagesProdutos/detalhesPizza.html?pizzaid=${pizzaId}`;
         });
     });
-}
-
-await renderPizza();
+}renderPizza();
 
 
 async function renderHamburguer() {
@@ -69,12 +67,12 @@ async function renderHamburguer() {
         hamburguerElement.className = 'w-[155px] h-[222px] relative bg-white shadow rounded-xl mx-7';
 
         const hamburguerImage = document.createElement('img');
-        hamburguerImage.classList = 'w-[134.80px] h-[116.21px] left-[10px] top-[20px] absolute'
+        hamburguerImage.className = 'w-[134.80px] h-[116.21px] left-[10px] top-[20px] absolute'
         hamburguerImage.src = hamburguerData.pathPhoto;
         hamburguerElement.appendChild(hamburguerImage);
 
         const hamburguerName = document.createElement('p');
-        hamburguerName.className = 'w-[86px] h-[15px] left-[35px] top-[140px] absolute text-center text-xl font-semibold fontText';
+        hamburguerName.className = 'h-[15px] top-[140px] relative mx-auto text-center text-xl font-semibold fontText';
         hamburguerName.textContent = hamburguerData.nome;
         hamburguerElement.appendChild(hamburguerName);
 
@@ -107,3 +105,53 @@ async function renderHamburguer() {
 }
 
 renderHamburguer();
+
+async function renderDoces() {
+    const sectionDoces = document.getElementById('sectionDoces');
+    const docesRef = db.collection('Doces');
+
+    const querySnapshot = await docesRef.get();
+    querySnapshot.forEach((doc) => {
+        docesData = doc.data();
+        docesId = doc.id;
+
+        const docesElement = document.createElement('div');
+        docesElement.className = 'w-[155px] h-[222px] relative bg-white shadow rounded-xl mx-7';
+
+        const docesImage = document.createElement('img');
+        docesImage.className = 'w-[149.81px] h-[100px] top-[20px] absolute'
+        docesImage.src = docesData.pathPhoto;
+        docesElement.appendChild(docesImage);
+
+        const docesName = document.createElement('p');
+        docesName.className = 'w-[137px] h-[15px] mx-auto top-[140px] relative text-center text-xl font-semibold fontText';
+        docesName.textContent = docesData.nome;
+        docesElement.appendChild(docesName);
+
+        const docePriceDiv = document.createElement('div');
+        docePriceDiv.className = 'left-[16px] top-[178px] absolute text-center ';
+        const docePrice = document.createElement('p');
+        docePrice.className = 'text-amber-400 text-xl font-semibold fontText';
+        docePrice.textContent = `R$ ${docesData.preco}`;
+
+        docePriceDiv.appendChild(docePrice);
+        docesElement.appendChild(docePriceDiv);
+
+        const iconAddFirstDiv = document.createElement('div');
+        iconAddFirstDiv.className = 'w-[25px] h-[25px] left-[118px] top-[185px] absolute';
+        const iconAddSecondDiv = document.createElement('div');
+        iconAddSecondDiv.className = 'w-[25px] h-[25px] left-0 top-0 absolute'
+        const iconAdd = document.createElement('img');
+        iconAdd.src = '../iconsFood/add_roudond.svg';
+
+        iconAddSecondDiv.appendChild(iconAdd);
+        iconAddFirstDiv.appendChild(iconAddSecondDiv);
+        docesElement.appendChild(iconAddFirstDiv);
+
+        sectionDoces.appendChild(docesElement);
+
+        docesElement.addEventListener('click', ()=>{
+            window.location.href = `../pagesProdutos/detalhesDoces.html?docesid=${docesId}`;
+        })
+    })
+} renderDoces();
