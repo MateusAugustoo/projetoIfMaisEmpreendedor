8a76db1a9282dc026c71a98ad1bb266967b3b0a6
MateusAugusto <mateusaugusto2017pp@gmail.com>
2023-11-19 22:39:22 -0300

    modele inicia do card bebidas

---
+++ public/js/bebidas/renderBebidas.js
@@ -0,0 +1,60 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAMYwMahCex-tSp-g7eEW5uENub_EmvMU",
+    authDomain: "ifmaisempreendedorangica-4cab5.firebaseapp.com",
+    projectId: "ifmaisempreendedorangica-4cab5",
+    storageBucket: "ifmaisempreendedorangica-4cab5.appspot.com",
+    messagingSenderId: "971295144283",
+    appId: "1:971295144283:web:35965497f0a0fe43b6427e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const sectionBebidas = document.getElementById('sectionBebidas');
+
+const renderCradBebidas = () => {
+    const container = document.createElement('div');
+    container.id = 'cardBebidas'
+    container.className = 'w-[331px] h-[222px] relative mx-auto';
+
+    const containerColorBackground = document.createElement('div');
+    containerColorBackground.className = 'w-[331px] h-[222px] relative mx-auto mt-5 bg-white rounded-[20px] shadow';
+    container.appendChild(containerColorBackground);
+
+    const containerImageAdd = document.createElement('button');
+    containerImageAdd.className = 'w-[30px] h-[30px] left-[294px] top-[185px] absolute';
+
+    const imageIconAdd = document.createElement('img');
+    imageIconAdd.className = 'w-[30px] h-[30px] left-[294px] top-[185px] absolute';
+    imageIconAdd.src = '../../iconsFood/add_roudond.svg';
+    imageIconAdd.alt = 'icone de adicionar bebidas';
+    containerImageAdd.appendChild(imageIconAdd);
+    containerColorBackground.appendChild(containerImageAdd);
+
+    const titleCard = document.createElement('p');
+    titleCard.className = 'w-[133px] left-[188px] top-[13px] absolute text-xl font-semibold fontText';
+    titleCard.textContent = 'Refrigerante em Lata'
+    containerColorBackground.appendChild(titleCard);
+
+    const descriptionCard = document.createElement('p');
+    descriptionCard.className = 'w-[122px] left-[190px] top-[76px] absolute text-zinc-600 text-[13px] font-bold fontText';
+    descriptionCard.id = 'descriptionCard';
+    containerColorBackground.appendChild(descriptionCard);
+
+    const priceCard = document.createElement('p');
+    priceCard.className = 'left-[23px] top-[178px] absolute text-center text-amber-400 text-xl font-bold fontText tracking-wide';
+    priceCard.id = 'priceCard';
+    containerColorBackground.appendChild(priceCard);
+
+    const image = document.createElement('img');
+    image.className = 'w-[175px] h-[152px] left-[10px] top-[26px] absolute';
+    image.src = '../../images/refriLata.jpg';
+    image.alt = 'imagem de refrigerantes em lata';
+    containerColorBackground.appendChild(image);
+
+    sectionBebidas.appendChild(container);
+};
+renderCradBebidas();
+
