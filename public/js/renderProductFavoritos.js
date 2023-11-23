724dda4b72584df47453d7176b3b6b7df183186a
MateusAugusto <mateusaugusto2017pp@gmail.com>
2023-11-22 22:44:04 -0300

    Refactor pizzaElement to hamElement in renderizarFavoritos function

--- public/js/renderProductFavoritos.js
+++ public/js/renderProductFavoritos.js
@@ -54,6 +54,12 @@ function renderizarFavoritos(favoritos) {
                 pizzaCircle.className = 'w-[142.86px] h-[142px] left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]';
                 pizzaElement.appendChild(pizzaCircle);
 
+                const pizzaImg = document.createElement('img');
+                pizzaImg.className = 'flex justify-center items-center relative w-auto h-auto';
+                pizzaImg.src = lancheData.imgPath;
+
+                pizzaCircle.appendChild(pizzaImg);
+
                 const pizzaPrice = document.createElement('div');
                 pizzaPrice.className = 'w-[90.47px] left-[14.08px] top-[108px] absolute text-center';
                 pizzaElement.appendChild(pizzaPrice);
@@ -86,33 +92,41 @@ function renderizarFavoritos(favoritos) {
                 const lancheData = doc.data()
                 const lancheId = doc.id
                 //card de favoritos para a Hamburguer
-                const pizzaElement = document.createElement('div');
-                pizzaElement.className = 'w-[333px] h-[142px] relative mx-8 mt-5';
+                const hamElement = document.createElement('div');
+                hamElement.className = 'w-[333px] h-[142px] relative mx-8 mt-5';
 
-                const pizzaBackground = document.createElement('div');
-                pizzaBackground.className = 'w-[333px] h-[142px] left-0 top-0 absolute bg-white rounded-[20px] shadow';
-                pizzaElement.appendChild(pizzaBackground);
+                const hamBackground = document.createElement('div');
+                hamBackground.className = 'w-[333px] h-[142px] left-0 top-0 absolute bg-white rounded-[20px] shadow';
+                hamElement.appendChild(hamBackground);
 
-                const pizzaCircle = document.createElement('div');
-                pizzaCircle.className = 'w-[142.86px] h-[142px] left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]';
-                pizzaElement.appendChild(pizzaCircle);
+                const hamCircle = document.createElement('div');
+                hamCircle.className = 'w-[142.86px] h-[142px] left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]';
 
-                const pizzaPrice = document.createElement('div');
-                pizzaPrice.className = 'w-[90.47px] left-[14.08px] top-[108px] absolute text-center';
-                pizzaElement.appendChild(pizzaPrice);
+                const hamImg = document.createElement('img');
+                hamImg.className = 'flex justify-center items-center relative w-[121.32px] h-[107px] mx-auto mt-5';
+                hamImg.src = lancheData.imgPath;
 
-                const pizzaFlavor = document.createElement('div');
-                pizzaFlavor.className = 'w-auto left-[22.13px] top-[15px] absolute text-orange-600 text-xl font-semibold fontText';
-                pizzaFlavor.textContent = lancheData.nome;
-                pizzaElement.appendChild(pizzaFlavor);
-
-                const pizzaIngredients = document.createElement('div');
-                pizzaIngredients.className = 'w-[134.81px] left-[22.13px] top-[47px] absolute text-zinc-600 text-[8px] font-bold fontText';
-                pizzaIngredients.textContent = lancheData.ingredientes;
-                pizzaElement.appendChild(pizzaIngredients);
+                hamCircle.appendChild(hamImg);
 
 
-                sectionFavorita.appendChild(pizzaElement);
+                hamElement.appendChild(hamCircle);
+
+                const hamPrice = document.createElement('div');
+                hamPrice.className = 'w-[90.47px] left-[14.08px] top-[108px] absolute text-center';
+                hamElement.appendChild(hamPrice);
+
+                const hamFlavor = document.createElement('div');
+                hamFlavor.className = 'w-auto left-[22.13px] top-[15px] absolute text-orange-600 text-xl font-semibold fontText';
+                hamFlavor.textContent = lancheData.nome;
+                hamElement.appendChild(hamFlavor);
+
+                const hamIngredients = document.createElement('div');
+                hamIngredients.className = 'w-[134.81px] left-[22.13px] top-[47px] absolute text-zinc-600 text-[8px] font-bold fontText';
+                hamIngredients.textContent = lancheData.ingredientes;
+                hamElement.appendChild(hamIngredients);
+
+
+                sectionFavorita.appendChild(hamElement);
             }
         } catch (error) {
 
