9c81bfe62117eb5c2168312f2102aa975c71a7e9
Erick Alves <britoerick16@gmail.com>
2023-12-07 21:18:25 -0300

    Login com Google

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,7 @@ firebase.initializeApp(window.firebaseConfig);
 
 const auth = firebase.auth();
 
-
-//Login via Email e Senha 
+//Login via Email e Senha
 const formLogin = document.getElementById("formLogin");
 formLogin.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -11,25 +10,28 @@ formLogin.addEventListener("submit", function (event) {
   const email = document.querySelector('[name="emailLogin"]').value;
   const senha = document.querySelector('[name="senhaLogin"]').value;
 
-  auth
-    .signInWithEmailAndPassword(email, senha)
-    .then(() => {
-      const user = auth.currentUser;
-      const userId = user.uid;
-      console.log(userId);
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          window.location.href = `../screens/home.html?uid=${userId}`;
-        }
-      })
-    })
-    .catch(() => {
-      alert("Email ou senha incorretos");
-    });
+  auth.signInWithEmailAndPassword(email, senha).catch(() => {
+    alert("Email ou senha incorretos");
+  });
 });
 
-
 //Login Via Google
-
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const btnGoogle = document.getElementById("google");
+btnGoogle.addEventListener("click", function (event) {
+  auth.signInWithRedirect(googleProvider);
+});
 
 //Login Via Facebook
+
+// Redirecionar 
+auth.getRedirectResult(auth).then((_) => {
+  const user = auth.currentUser;
+  const userId = user.uid;
+  console.log(userId);
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      window.location.href = `../screens/home.html?uid=${userId}`;
+    }
+  });
+});
