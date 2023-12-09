d0aeb2f720908d108f8629b2aca01585c5ed4c47
Erick Alves <britoerick16@gmail.com>
2023-12-09 11:40:47 -0300

    Refatoração Page dos Favoritos

--- public/js/renderProductFavoritos.js
+++ public/js/renderProductFavoritos.js
@@ -2,127 +2,90 @@ firebase.initializeApp(window.firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
-
 auth.onAuthStateChanged(async function (user) {
-    if (user) {
-        const userId = user.uid;
-        const favoritoRef = db.collection('Favoritos').doc(userId)
+  if (user) {
+    const userId = user.uid;
+    const favoritoRef = db.collection("Favoritos").doc(userId);
 
-        try {
-            const doc = await favoritoRef.get();
-            if (doc.exists) {
-                const favoritosData = doc.data()
-                const favoritos = favoritosData.lanches
+    try {
+      const doc = await favoritoRef.get();
+      if (doc.exists) {
+        const favoritosData = doc.data();
+        const favoritos = favoritosData.lanches;
 
-                renderizarFavoritos(favoritos);
-            }
-        } catch (error) {
-            console.error(error);
-        }
+        renderizarFavoritos(favoritos);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 });
 
-function renderizarFavoritos(favoritos) {
-    const sectionFavorita = document.getElementById('sectionFavoritos')
+async function renderizarFavoritos(favoritos) {
+  const sectionFavorita = document.getElementById("sectionFavoritos");
+  const tiposLanche = ["Pizza", "Hamburguer", "Doces"];
 
-    favoritos.forEach(async (lancheId) => {
-        const pizzaRef = db.collection('Pizza').doc(lancheId);
+  for (const lancheId of favoritos) {
+    const promises = tiposLanche.map(tipo => db.collection(tipo).doc(lancheId).get());
+    try {
+      const docs = await Promise.all(promises);
+      docs.forEach((doc, index) => {
+        if (doc.exists) {
+          const tipo = tiposLanche[index];
+          const lancheElement = criarElement(doc);
 
-        try {
-            const doc = await pizzaRef.get();
-            if (doc.exists) {
-                const lancheData = doc.data()
-                const lancheId = doc.id
-                //refazer o card de favoritos para a Pizza
-                const pizzaElement = document.createElement('div');
-                pizzaElement.className = 'w-[333px] h-[142px] relative mx-8 mt-5 mx-auto';
+          lancheElement.addEventListener("click", () => {
+            window.location.href = `../pagesProdutos/${tipo.toLowerCase()}/detalhes${tipo}.html?lancheId=${lancheId}`;
+          });
 
-                const pizzaBackground = document.createElement('div');
-                pizzaBackground.className = 'w-[333px] h-[142px] left-0 top-0 absolute bg-white rounded-[20px] shadow';
-                pizzaElement.appendChild(pizzaBackground);
-
-                const pizzaCircle = document.createElement('div');
-                pizzaCircle.className = 'w-[142.86px] h-[142px] left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]';
-                pizzaElement.appendChild(pizzaCircle);
-
-                const pizzaImg = document.createElement('img');
-                pizzaImg.className = 'flex justify-center items-center relative w-auto h-auto';
-                pizzaImg.src = lancheData.imgPath;
-
-                pizzaCircle.appendChild(pizzaImg);
-
-                const pizzaPrice = document.createElement('div');
-                pizzaPrice.className = 'w-[90.47px] left-[14.08px] top-[108px] absolute text-center';
-                pizzaElement.appendChild(pizzaPrice);
-
-                const pizzaFlavor = document.createElement('div');
-                pizzaFlavor.className = 'w-auto left-[22.13px] top-[15px] absolute text-orange-600 text-xl font-semibold fontText';
-                pizzaFlavor.textContent = lancheData.nome;
-                pizzaElement.appendChild(pizzaFlavor);
-
-                const pizzaIngredients = document.createElement('div');
-                pizzaIngredients.className = 'w-[134.81px] left-[22.13px] top-[47px] absolute text-zinc-600 text-[8px] font-bold fontText';
-                pizzaIngredients.textContent = lancheData.ingredientes;
-                pizzaElement.appendChild(pizzaIngredients);
-
-                pizzaElement.addEventListener('click', () => {
-                    window.location.href = `../pagesProdutos/pizza/detalhesPizza.html?lancheId=${lancheId}`
-                })
-
-                sectionFavorita.appendChild(pizzaElement);
-            }
-        } catch (error) {
-            console.error(error);
+          sectionFavorita.appendChild(lancheElement);
         }
-
-        const hamburguerRef = db.collection('Hamburguer').doc(lancheId);
-
-        try {
-            const doc = await hamburguerRef.get();
-            if (doc.exists) {
-                const lancheData = doc.data()
-                const lancheId = doc.id
-                //card de favoritos para a Hamburguer
-                const hamElement = document.createElement('div');
-                hamElement.className = 'w-[333px] h-[142px] relative mx-8 mt-5 mx-auto';
-
-                const hamBackground = document.createElement('div');
-                hamBackground.className = 'w-[333px] h-[142px] left-0 top-0 absolute bg-white rounded-[20px] shadow';
-                hamElement.appendChild(hamBackground);
-
-                const hamCircle = document.createElement('div');
-                hamCircle.className = 'w-[142.86px] h-[142px] left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]';
-
-                const hamImg = document.createElement('img');
-                hamImg.className = 'flex justify-center items-center relative w-[121.32px] h-[107px] mx-auto mt-5';
-                hamImg.src = lancheData.imgPath;
-
-                hamCircle.appendChild(hamImg);
-
-
-                hamElement.appendChild(hamCircle);
-
-                const hamPrice = document.createElement('div');
-                hamPrice.className = 'w-[90.47px] left-[14.08px] top-[108px] absolute text-center';
-                hamElement.appendChild(hamPrice);
-
-                const hamFlavor = document.createElement('div');
-                hamFlavor.className = 'w-auto left-[22.13px] top-[15px] absolute text-orange-600 text-xl font-semibold fontText';
-                hamFlavor.textContent = lancheData.nome;
-                hamElement.appendChild(hamFlavor);
-
-                const hamIngredients = document.createElement('div');
-                hamIngredients.className = 'w-[134.81px] left-[22.13px] top-[47px] absolute text-zinc-600 text-[8px] font-bold fontText';
-                hamIngredients.textContent = lancheData.ingredientes;
-                hamElement.appendChild(hamIngredients);
-
-
-                sectionFavorita.appendChild(hamElement);
-            }
-        } catch (error) {
-
-        }
-
-    });
+      });
+    } catch (error) {
+      console.error(`Erro ao buscar lanche com ID ${lancheId}: `, error);
+    }
+  }
 }
 
+function criarElement(doc) {
+  const lancheData = doc.data();
+
+  const LancheElement = document.createElement("div");
+  LancheElement.className = "w-[333px] h-[142px] relative mx-8 mt-5 mx-auto";
+
+  const LancheBackground = document.createElement("div");
+  LancheBackground.className =
+    "w-[333px] h-[142px] left-0 top-0 absolute bg-white rounded-[20px] shadow";
+  LancheElement.appendChild(LancheBackground);
+
+  const LancheCircle = document.createElement("div");
+  LancheCircle.className =
+    "w-[142.86px] h-[142px] flex justify-center items-center left-[190.14px] top-0 absolute bg-orange-600 rounded-[20px]";
+
+  const LancheImg = document.createElement("img");
+  LancheImg.className =
+    "flex justify-center items-center relative w-36 mx-auto mt-5";
+  LancheImg.src = lancheData.imgPath;
+
+  LancheCircle.appendChild(LancheImg);
+
+  LancheElement.appendChild(LancheCircle);
+
+  const LanchePrice = document.createElement("div");
+  LanchePrice.className =
+    "w-[90.47px] left-[14.08px] top-[108px] absolute text-center";
+  LancheElement.appendChild(LanchePrice);
+
+  const LancheFlavor = document.createElement("div");
+  LancheFlavor.className =
+    "w-auto left-[22.13px] top-[15px] absolute text-orange-600 text-xl font-semibold fontText";
+  LancheFlavor.textContent = lancheData.nome;
+  LancheElement.appendChild(LancheFlavor);
+
+  const LancheIngredients = document.createElement("div");
+  LancheIngredients.className =
+    "w-[134.81px] left-[22.13px] top-[47px] absolute text-zinc-600 text-[8px] font-bold fontText";
+  LancheIngredients.textContent = lancheData.ingredientes;
+  LancheElement.appendChild(LancheIngredients);
+  return LancheElement;
+}
